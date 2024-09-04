03281d1b55e4a74d24d30d06ac2df71d0f223dbb
Eduardo <narutuuo3132@gmail.com>
2024-09-04 22:23:27 +0900

    dicas  do eduardo

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = 5;
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag , texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('P', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('P', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
